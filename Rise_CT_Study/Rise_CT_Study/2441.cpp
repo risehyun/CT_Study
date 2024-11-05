77314ecff79ca297b6245e9605ea549809a97a28
risehyun <risehyun01@gmail.com>
2024-11-05 22:57:01 +0900

    [ADD] 2441

---
+++ Rise_CT_Study/Rise_CT_Study/2441.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N = 0;
+	cin >> N;
+
+	for (int y = 0; y < N; y++)
+	{
+		for (int i = 0; i < y; i++)
+		{
+			cout << ' ';
+		}
+
+		for (int x = N - y; x > 0; x--)
+		{
+			cout << '*';
+		}
+
+		cout << '\n';
+	}
+
+	return 0;
+}
