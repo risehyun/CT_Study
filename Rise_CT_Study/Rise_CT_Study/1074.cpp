ed005c9463b6ca19d5000262285c610491e1e880
risehyun <risehyun01@gmail.com>
2024-11-26 21:45:50 +0900

    [ADD]

--- Rise_CT_Study/Rise_CT_Study/1074.cpp
+++ Rise_CT_Study/Rise_CT_Study/1074.cpp
@@ -1,48 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int func(int n, int r, int c)
-{
-	if (n == 0)
-	{
-		return 0;
-	}
-
-	// 1을 비트 기준으로 n - 1칸 밀어줌 == 2^k
+int func(int n, int r, int c) {
+	if (n == 0) return 0;
 	int half = 1 << (n - 1);
-
-	// 왼쪽 위
-	if (r < half && c < half)
-	{
-		return func(n - 1, r, c);
-	}
-
-	// 왼쪽 아래
-	if (r < half && c >= half)
-	{
-		half* half + func(n - 1, r, c - half);
-	}
-
-	// 오른쪽 위
-	if (r >= half && c < half)
-	{
-		return 2 * half * half + func(n - 1, r - half, c);
-	}
-
-	// 오른쪽 아래
+	if (r < half && c < half) return func(n - 1, r, c);
+	if (r < half && c >= half) return half * half + func(n - 1, r, c - half);
+	if (r >= half && c < half) return 2 * half * half + func(n - 1, r - half, c);
 	return 3 * half * half + func(n - 1, r - half, c - half);
 }
 
-int main()
-{
-	ios::sync_with_stdio(false);
+int main(void) {
+	ios::sync_with_stdio(0);
 	cin.tie(0);
-
-	int n = 0, r = 0, c = 0;
-
+	int n, r, c;
 	cin >> n >> r >> c;
-
 	cout << func(n, r, c);
-
-	return 0;
 }
