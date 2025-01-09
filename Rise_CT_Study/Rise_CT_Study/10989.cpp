1ef191379303d9afd606a6c4418b980ac8d1611f
risehyun <risehyun01@gmail.com>
2025-01-09 21:44:02 +0900

    [ADD]

--- Rise_CT_Study/Rise_CT_Study/10989.cpp
+++ Rise_CT_Study/Rise_CT_Study/10989.cpp
@@ -1,31 +1,31 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int arr[10001];
-//int input, n;
-//
-//int main()
-//{
-//	ios::sync_with_stdio(false);
-//	cin.tie(0);
-//
-//	cin >> n;
-//
-//	for (int i = 0; i < n; i++)
-//	{
-//		cin >> input;
-//		arr[input]++;
-//	}
-//
-//	for (int i = 1; i <= 10000; i++)
-//	{
-//		while (arr[i])
-//		{
-//			cout << i << '\n';
-//			arr[i]--;
-//		}
-//	}
-//
-//	return 0;
-//}
+#include <iostream>
+
+using namespace std;
+
+int arr[10001];
+int input, n;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input;
+		arr[input]++;
+	}
+
+	for (int i = 1; i <= 10000; i++)
+	{
+		while (arr[i])
+		{
+			cout << i << '\n';
+			arr[i]--;
+		}
+	}
+
+	return 0;
+}
--- Rise_CT_Study/Rise_CT_Study/2751.cpp
+++ Rise_CT_Study/Rise_CT_Study/2751.cpp
@@ -1,34 +1,34 @@
-//// 수 정렬하기 2
-//
-//#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	ios::sync_with_stdio(false);
-//	cin.tie(0);
-//
-//	int n = 0;
-//	vector<int> input;
-//
-//	cin >> n;
-//
-//	input.resize(n);
-//
-//	for (int i = 0; i < n; i++)
-//	{
-//		cin >> input[i];
-//	}
-//
-//	sort(input.begin(), input.end());
-//
-//	for (int i = 0; i < n; i++)
-//	{
-//		cout << input[i] << '\n';
-//	}
-//
-//	return 0;
-//}
+// 수 정렬하기 2
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n = 0;
+	vector<int> input;
+
+	cin >> n;
+
+	input.resize(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input[i];
+	}
+
+	sort(input.begin(), input.end());
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << input[i] << '\n';
+	}
+
+	return 0;
+}
