daa51d4f3db517583280589170209781e9c5c4f6
DahyunKim <risehyun01@gmail.com>
2025-01-28 23:43:32 +0900

    [ADD]

---
+++ Rise_CT_Study/Rise_CT_Study/11659.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//using namespace std;
+//
+//int n, m;
+//int a[100004], d[100004];
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
+//	cin >> n >> m;
+//
+//	d[0] = 0;
+//
+//	for (int i = 1; i <= n; i++)
+//	{
+//		cin >> a[i];
+//		d[i] = d[i - 1] + a[i];
+//	}
+//
+//	while (m--)
+//	{
+//		int i, j;
+//		cin >> i >> j;
+//		cout << d[j] - d[i - 1] << '\n';
+//	}
+//
+//	return 0;
+//}
